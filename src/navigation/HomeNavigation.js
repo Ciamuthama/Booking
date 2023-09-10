645aaaa4dd262c9c7844dd8a4f3ad6265b251419
Ciamuthama <muthamapeter79@gmail.com>
2023-09-10 18:06:00 +0300

    Added to more screen and bottom tabs 😊

--- src/navigation/HomeNavigation.js
+++ src/navigation/HomeNavigation.js
@@ -3,7 +3,9 @@ import { View, Text } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import BookingScreen from "../screens/BookingScreen.js";
 import TripScreen from "../screens/TripScreen";
+import CheckInScreen from "../screens/CheckInScreen.js";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,16 +13,18 @@ function HomeNavigation() {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarShowLabel: false,
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor:'black',
+        tabBarLabelStyle:{fontSize:15, fontWeight:'600',},
         tabBarStyle: {
           position: "absolute",
-          bottom: 25,
+          bottom: 20,
           left: 10,
           right: 10,
           justifyContent: "center",
           alignItem: "center",
           flex: 1,
-          paddingBottom: 5,
+          paddingBottom: 15,
           height: 90,
           borderTopWidth: 0,
           elevation: 0,
@@ -34,48 +38,82 @@ function HomeNavigation() {
         options={{
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <View className="flex justify-center  bg-orange-300 p-3">
+            <View className="flex justify-center -mb-5">
               <Ionicons
                 name={focused ? "md-home-sharp" : "md-home-outline"}
-                size={28}
+                size={24}
                 color="black"
-                className="justify-center flex align-middle w-full"
+                
               />
-              <Text className="font-bold text-center">Home</Text>
+              
             </View>
           ),
         }}
         component={HomeScreen}
+      />
+       <Tab.Screen
+        name="Book Flight"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <View className="flex justify-center align-middle -mb-5">
+              <Ionicons
+                name={focused ? "airplane-sharp" : "airplane-outline"}
+                size={24}
+                color="black"
+              />
+            </View>
+          ),
+        }}
+        component={BookingScreen}
       />
       <Tab.Screen
         name="Trips"
         options={{
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <View className="flex justify-center align-middle">
+            <View className="flex justify-center align-middle -mb-5">
               <Ionicons
                 name={focused ? "ios-briefcase-sharp" : "ios-briefcase-outline"}
-                size={28}
+                size={24}
                 color="black"
               />
-              <Text>My Trips</Text>
+             
             </View>
           ),
         }}
         component={TripScreen}
       />
+       <Tab.Screen
+        name="Check In"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <View className="flex justify-center align-middle -mb-5">
+              <Ionicons
+                name={focused ? "bus" : "bus-outline"}
+                size={24}
+                color="black"
+              />
+            </View>
+          ),
+        }}
+        component={CheckInScreen}
+      />
       <Tab.Screen
         name="Profile"
         options={{
           headerShown: false,
+         
           tabBarIcon: ({ focused }) => (
-            <View>
+            <View className='flex justify-center align-middle -mb-5'>
               <Ionicons
                 name={focused ? "md-person" : "md-person-outline"}
-                size={28}
+                size={24}
                 color="black"
+                
               />
-              <Text>Profile</Text>
+             
             </View>
           ),
         }}
---
+++ src/screens/BookingScreen.js
@@ -0,0 +1,12 @@
+import { Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+
+export default function BookingScreen() {
+ 
+  return (
+    <SafeAreaView className='bg-gray flex justify-center align-middle mt-6 '>
+          <Text className='m-1 text-center'>BookingScreen</Text>
+    </SafeAreaView>
+  )
+}
