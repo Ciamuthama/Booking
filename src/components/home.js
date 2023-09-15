2d5fa79743eeefc36c2550daac94dd32b7245918
Ciamuthama <muthamapeter79@gmail.com>
2023-09-15 18:18:05 +0300

    Added Image Data to a flatlist to render destinations

--- src/components/home.js
+++ src/components/home.js
@@ -1,23 +1,28 @@
-import { Image, Pressable, ScrollView, Text,View } from 'react-native'
+import { Image, Pressable, ScrollView, Text,View,FlatList } from 'react-native'
 import React from 'react'
 import { widthPercentageToDP as wp ,heightPercentageToDP as hp } from "react-native-responsive-screen";
 import QuickTabs from './QuickTabs'
+import { Data } from '../constants/ExproleData';
 
 export default function Home() {
-const imageUrl= 'https://drscdn.500px.org/photo/241157549/q%3D80_m%3D1000/v2?sig=83315a1fc8f34237ec2f924c1f1d34eb308f8958c3764b449092bf37db4bb477'
 
   return (
     <ScrollView className='space-x-2'>
       <View>
           <QuickTabs />
       </View>
-      <View>
-       
-        <Pressable className='bg-slate-200 mt-5 rounded-2xl mr-2 shadow-2xl'>
-          <Image source={{uri:imageUrl}} className='rounded-2xl relative ' style={{width:wp(50),height:wp(40)}}  />
-          <Text className='absolute bottom-1 w-20 rounded-2xl text-center'>name</Text>
+      <FlatList>
+       {
+          Data.map((key, index) => (
+         
+        <Pressable key={index} className='bg-slate-200 mt-5 rounded-2xl mr-2 shadow-2xl'>
+          <Image source={{uri:key.image}} className='rounded-2xl relative ' style={{width:wp(50),height:wp(40)}}  />
+              <Text className='absolute bottom-1 w-20 rounded-2xl text-center'>{ key.title}</Text>
         </Pressable>
-      </View>    
+            
+       ))
+       }
+      </FlatList>    
     </ScrollView>
   )
 }
---
+++ src/constants/ExproleData.js
@@ -0,0 +1,103 @@
+export  const Data = [
+    {
+        title:'Kisumu City',
+        image: 'https://drscdn.500px.org/photo/241157549/q%3D80_m%3D1000/v2?sig=83315a1fc8f34237ec2f924c1f1d34eb308f8958c3764b449092bf37db4bb477',
+
+    },
+    {
+        title:'Kilimanjaro',
+        image:'https://drscdn.500px.org/photo/78066107/m%3D900/v2?sig=f7d3c4baca50f97366aac07afcbcce78052d6aaa472db37a28b4e36d67af3d4a',
+    },
+    {
+        title: 'Entebbe',
+        image:'https://drscdn.500px.org/photo/111047983/m%3D900/v2?sig=7c85576d6a279cd2f91e6e2ec853ff460109c905e400b16d6df8df67796998ab'
+    },
+    {
+        title:'Kampala',
+        image:'https://images.unsplash.com/photo-1578325872347-6cc1795a5fea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        title: 'Mombasa',
+        image: 'https://images.unsplash.com/photo-1554732257-a16cfdfec4b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9tYmFzYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
+    },
+    {
+        title:'Dar es salaam',
+        image: 'https://images.unsplash.com/photo-1589177900326-900782f88a55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Kigali',
+        image:'https://images.unsplash.com/photo-1689013398932-b576a11e07a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title:'Zanzibar',
+        image:'https://images.unsplash.com/photo-1549035092-33b2937b075a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Dubai',
+        image:'https://images.unsplash.com/photo-1518684079-3c830dcef090?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Lilongwe',
+        image:'https://drscdn.500px.org/photo/132282621/m%3D900/v2?sig=cc897c135df88c08ba90f891c1e708831295c7ec60b3978284da37bfa64cdc76'
+    },
+    {
+        title: 'Mumbai',
+        image:'https://images.unsplash.com/photo-1591014141178-02091240f1c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Cape Town',
+        image:'https://images.unsplash.com/photo-1580060839134-75a5edca2e99?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Johannesburg',
+        image: 'https://images.unsplash.com/photo-1636706519609-988babca3dd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Accra',
+        image:'https://images.unsplash.com/photo-1630386226447-af0a955c1009?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Addis Ababa',
+        image: 'https://drscdn.500px.org/photo/1003870226/m%3D900/v2?sig=7847f43a94deb7e59c840ac4ce6a1252ce593e120fdadbf502051978fe271eb4',
+    },
+    {
+        title: 'Abidjan',
+        image:'https://drscdn.500px.org/photo/170554335/m%3D900/v2?sig=c5bbdc6cf19164b2d5827d55fdd46c9e46756a9c0e28b81a6bfab7aae923ef99',
+    },
+    {
+        title: 'Kinshasa',
+        image:'',
+    },
+    {
+        title:'Mahe Island',
+        image:'https://drscdn.500px.org/photo/118463653/m%3D900/v2?sig=359fd0ea43b159c32b64743d7079ba3bdaa710d66412d15913c4e60dc0d562d3'
+    },
+    {
+        title: "GuangzHou",
+        image:'https://images.unsplash.com/photo-1563090162-6b4c2a20d658?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        title: 'London',
+        image:'https://images.unsplash.com/photo-1533929736458-ca588d08c8be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        title: 'Lagos',
+        image:'https://images.unsplash.com/photo-1618828665347-d870c38c95c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        title: 'Victoria Falls',
+        image:'https://images.unsplash.com/photo-1603201236596-eb1a63eb0ede?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        title: 'Dakar',
+        image:'https://images.unsplash.com/photo-1611258490565-4a06c019e631?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Livingstone',
+        image:'https://images.unsplash.com/photo-1666732566977-8805c13a6ce2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+        title: 'Paris',
+        image:'https://images.unsplash.com/photo-1500313830540-7b6650a74fd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    }
+]
