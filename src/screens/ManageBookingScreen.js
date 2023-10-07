99403898a93d1ba2d335f1c55740defeaa0f9258
Ciamuthama <muthamapeter79@gmail.com>
2023-10-07 11:33:42 +0300

    Added a barcode that will be unique for every ticket/baording pass

--- src/screens/ManageBookingScreen.js
+++ src/screens/ManageBookingScreen.js
@@ -6,48 +6,57 @@ import {
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { LinearGradient } from "expo-linear-gradient";
-import Barcode from 'react-barcode';
+import Barcode from '../components/Barcode';
 
 export default function ManageBookingScreen() {
   return (
     <SafeAreaView>
-      
+
       <View className='flex mx-3 mt-2 rounded-xl bg-neutral-200 shadow-md'>
         <View>
-          <Image source={require('../../assets/image/kq2.png')} style={{width:wp(94.6),height:hp(50)}} className='rounded-t-xl' />
+          <Image source={require('../../assets/image/kq2.png')} style={{ width: wp(94.6), height: hp(50) }} className='rounded-t-xl' />
         </View>
         <LinearGradient
-              colors={["transparent", "rgb(229,229,229)"]}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 0, y: 1 }}
-              style={{ width: wp(94.6), height: hp(50) }}
-              className="absolute  z-10 rounded-t-xl"
-            />
+          colors={["transparent", "rgb(229,229,229)"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          style={{ width: wp(94.6), height: hp(50) }}
+          className="absolute  z-10 rounded-t-xl"
+        />
+        <View className='flex flex-row w-full'>
+          <Text>NRB</Text>
+          <Text>KIS</Text>
+        </View>
         <View className='flex flex-row justify-around items-center flex-wrap mx-10 '>
-      <View className='flex w-1/2 p-5'>
-      <Text className='text-black/60 text-lg font-bold'>Date & time</Text>
-      <Text className='text-black text-lg  font-semibold'>date</Text>
-      </View>
-      <View className='flex items-end w-1/2 p-5'>
-      <Text className='text-black/60 text-lg font-bold'>Flight Number</Text>
-      <Text className='text-black text-lg font-semibold'>date</Text>
-      </View>
-      <View className='flex  w-1/2 p-5'>
-      <Text className='text-black/60 text-lg font-bold'>Ticket price</Text>
-      <Text className='text-black text-lg font-semibold'>date</Text>
-      </View>
-      <View className='flex items-end w-1/2 p-5'>
-      <Text className='text-black/60 text-lg font-bold'>Class</Text>
-      <Text className='text-black text-lg font-semibold'>date</Text>
-      </View></View>
+          <View className='flex w-1/2 p-5'>
+            <Text className='text-black/60 text-lg font-bold'>Date & time</Text>
+            <Text className='text-black text-lg  font-semibold'>date</Text>
+          </View>
+          <View className='flex items-end w-1/2 p-5'>
+            <Text className='text-black/60 text-lg font-bold'>Flight Number</Text>
+            <Text className='text-black text-lg font-semibold'>date</Text>
+          </View>
+          <View className='flex  w-1/2 p-5'>
+            <Text className='text-black/60 text-lg font-bold'>Ticket price</Text>
+            <Text className='text-black text-lg font-semibold'>date</Text>
+          </View>
+          <View className='flex items-end w-1/2 p-5'>
+            <Text className='text-black/60 text-lg font-bold'>Class</Text>
+            <Text className='text-black text-lg font-semibold'>date</Text>
+          </View></View>
 
-      <View className='items-center border-t-[1px] border-dashed p-2'>
-        <View className='w-6 h-6 rounded-full bg-neutral-100 relative -top-5 -left-[219px]'></View>
-        <View className='w-6 h-6 rounded-full bg-neutral-100 relative -top-11 -right-[219px]'></View>
-          <Text className='text-black font-bold text-xl'>Boarding pass</Text>
-         
+        <View className='items-center border-t-[2px] border-dashed border-neutral-400 p-3'>
+          <View className='w-6 h-6 rounded-full bg-neutral-100 absolute -top-[13px] -left-[13px]'></View>
+          <View className='w-6 h-6 rounded-full bg-neutral-100 absolute -top-[13px] -right-[13px]'></View>
+          <Text className='text-black font-bold text-base'>Boarding pass</Text>
+
+          <Barcode
+            value="    "
+            options={{ format: 'CODE128', background: 'rgba(255,255,255,0.1)', height: wp(10), }}
+          />
+
+        </View>
       </View>
-      </View>
-   </SafeAreaView>
+    </SafeAreaView>
   )
 }
